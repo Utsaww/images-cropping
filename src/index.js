2844599df98abb76d5237e6c81a47e556cc0f742
Utsav Upadhyay <81962784+Utsaww@users.noreply.github.com>
2023-04-15 22:44:39 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import "./styles.css";
+import * as markerjs2 from "markerjs2";
+
+function showMarkerArea(target) {
+  const markerArea = new markerjs2.MarkerArea(target);
+  markerArea.addEventListener(
+    "render",
+    (event) => (target.src = event.dataUrl)
+  );
+  markerArea.show();
+}
+
+const sampleImage = document.getElementById("sampleImage");
+sampleImage.addEventListener("click", () => {
+  showMarkerArea(sampleImage);
+});
